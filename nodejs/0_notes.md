a8257f10317d48742fb9782f88dd3387d07f76aa
My name <zichuan.cecilia@gmail.com>
2024-06-06 23:05:45 -0400

    logger and vidly

---
+++ nodejs/0_notes.md
@@ -0,0 +1,96 @@
+# check node version
+
+node -- version
+
+# if haven't install node, go to nodejs.org
+
+# then run
+
+node app.js
+
+# scan js code errors
+
+jshint app.js
+
+# export object
+
+module.exports.log = log
+
+# export a single function
+
+module.exports = log
+
+# import module
+
+const logger = require('./logger'); # use './logger' for current folder, './subfolder/logger' for child folders and '../logger' for parent folder
+logger.log('message')
+
+# installing packages https://www.npmjs.com/
+
+npm i packagename
+npm i packagename@2.4.2
+
+npm i # install all packages in package.json
+npm i --only=prod # install only regular dependencies
+npm i --only=dev # install only dev dependencies
+npm i -g packagename # install package globally
+
+npm un mongoose # uninstall packages
+npm un -g mongoose
+
+# ignore node_modules with git
+
+create .gitignore
+node_modules/
+
+# list all installed packages and their dependencies
+
+npm list
+npm list --depth=0
+
+# viewing registry info for a package
+
+npm view packagename
+npm view mongoose dependencies
+npm view mongoose versions
+
+# check outdated packages
+
+npm outdated
+npm -g outdated
+npm update
+
+# update all to newest version
+
+npm i -g npm-check-updates # install cli tool to check updates globally
+npm-check-updates
+ncu -u # update package.json
+npm i
+
+# development dependencies, unit test, analyze js code, scan for errors etc, these should not go in production
+
+# scan js errors, specifiy development dependency
+
+npm i jshint --save-dev
+
+# publishing a package
+
+makedir lion-lib
+cd lion-lib
+npm init --yes #create package.json file, then create index.js and write function
+npm adduser # create account
+npm login # log in
+npm publish
+
+# update a published package, need to update version according to the update, can do it manually in the package.json file or run following
+
+npm version major
+npm version minor
+npm version patch
+npm publsih
+
+# setting environment variables
+
+export PORT=5000 # mac
+set PORT=5000 # windows command prompt
+$env:PORT=5000 # windows powershell
---
+++ nodejs/2_express-demo/index.js
@@ -0,0 +1,87 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required(), // name is required and should be minimum 3 characters
+  };
+
+  return Joi.validate(course, schema);
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found.");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const result = validateCourse(req.body);
+
+  if (result.error)
+    return res.status(400).send(result.error.details[0].message);
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  // Look up the course
+  // If not existing, return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found.");
+
+  // Validate
+  // If invalid, return 400 - Bad request
+  const { error } = validateCourse(req.body);
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+  }
+
+  // Update course
+  // Return the updated course
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  // Look up the course
+  // Not existing, return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found.");
+
+  // Delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1); // delete 1 element from index
+
+  // Return the same course
+  res.send(course);
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
